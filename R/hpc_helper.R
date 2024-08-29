e4184d1252863bbf38939724b645f0ea13a2e7a7
懒麻蛇 <frueddy@gmail.com>
2024-08-29 11:22:11 +0800

    dev

--- R/hpc_helper.R
+++ R/hpc_helper.R
@@ -44,6 +44,93 @@ brainscore.hpc <- function(job_id, n_perm_per_job = 1, perm_total, perm_id = NUL
   saveRDS(gs.score, file = file.path(output_dir, sprintf("res_job_%d.rds", job_id)))
 }
 
+
+#' Distribute and Process Iterations Across HPC Jobs
+#'
+#' This function distributes a set of iterations across multiple jobs in a high-performance computing (HPC) environment.
+#' It subsets the necessary variables, calls a specified function with the relevant subsets, and saves the results to a specified directory.
+#'
+#' @param job_id An integer specifying the job ID in the HPC job array (e.g., 1, 2, 3, ...). Determines which subset of iterations this job will process.
+#' @param n_iter_per_job An integer specifying the number of iterations each job should process. Default is 1.
+#' @param iter_total An integer specifying the total number of iterations to be processed across all jobs.
+#' @param subset_vars A named list of variables (typically matrices) that need to be subset according to the job's assigned iterations.
+#'        The names should correspond to the argument names in `FUN`.
+#' @param subset_total_var A character string specifying the name of the argument in `FUN` that corresponds to the total number of iterations.
+#'        If provided, the total number of iterations for the current job will be assigned to this argument.
+#' @param output_dir A character string specifying the directory where the results should be saved. If the directory does not exist, it will be created.
+#' @param FUN A function that will be called to process the data for the specified iterations. The function should accept the arguments specified in `...`.
+#' @param save_name A character string specifying the base name for the saved RDS files. Default is "res_job_".
+#' @param ... Additional arguments passed to `FUN`.
+#' @return This function does not return a value but saves the results of `FUN` to an RDS file in the specified `output_dir`.
+#' @examples
+#' \dontrun{
+#' # Call job_splitter, which will subset 'perm_id' and pass it to 'FUN'
+#' job_splitter(
+#'   job_id = 1,
+#'   n_iter_per_job = 10,
+#'   iter_total = 100,
+#'   output_dir = "/path/to/output",
+#'   FUN = brainscore,
+#'   subset_vars = list(perm_id = perm_id),
+#'   subset_total_var = "n_perm",
+#'   brain_data = brain_data,
+#'   gene_data = gene_data,
+#'   annoData = annoData,
+#'   cor_method = "pearson",
+#'   aggre_method = "mean",
+#'   null_model = "spin_brain",
+#'   minGSSize = 20,
+#'   maxGSSize = 200
+#' )
+#' }
+#' @export
+job_splitter <- function(job_id,
+                         n_iter_per_job = 1,
+                         iter_total,
+                         subset_vars = list(),
+                         subset_total_var = NULL,
+                         output_dir = NULL,
+                         FUN,
+                         save_name = "res_job_",
+                         ...) {
+  # Check if output_dir is provided and create if it doesn't exist
+  if (is.null(output_dir)) {
+    stop("output_dir must be specified.")
+  }
+
+  if (!dir.exists(output_dir)) {
+    message(sprintf("Output directory %s does not exist. Creating it...", output_dir))
+    dir.create(output_dir, recursive = TRUE)
+  }
+
+  # Calculate the number of iterations this job should handle
+  start_iter <- (job_id - 1) * n_iter_per_job + 1
+  end_iter <- min(job_id * n_iter_per_job, iter_total)
+  total_iter <- end_iter - start_iter + 1
+
+  # Subset the variables in subset_vars for this job
+  subset_vars_subset <- lapply(subset_vars, function(x) x[, start_iter:end_iter, drop = FALSE])
+
+  # Replace the original variables with their subsets
+  names(subset_vars_subset) <- names(subset_vars)
+
+  # Prepare the argument list for FUN
+  args <- c(subset_vars_subset, list(...))
+
+  # Set the total_iter_var in args if specified
+  if (!is.null(subset_total_var)) {
+    args[[subset_total_var]] <- total_iter
+  }
+  message(sprintf("Job %d processing iterations %d to %d...", job_id, start_iter, end_iter))
+  # Call the specified function with the arguments
+  result <- do.call(FUN, args)
+  # Save results to a file in the specified output directory
+  saveRDS(result, file = file.path(output_dir, sprintf("%s%d.rds", save_name, job_id)))
+}
+
+
+
+
 #' Combine Results from Saved RDS Files
 #'
 #' This function combines results from multiple saved RDS files into a single data frame or list,
