764df738080c817f4ef4a18a4c8bacf9e8f1ea45
zh1peng <frueddy@gmail.com>
2025-03-21 16:07:47 +0800

    startup msg

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -5,13 +5,13 @@
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Welcome to ", pkgname, "!")
-  packageStartupMessage(
-    "To cite the package, please use the following reference:\n",
-    "1. Cao, Z., Zhan, G., Qin, J., Cupertino, R. B., Ottino-Gonzalez, J., Murphy, A., ... & Garavan, H. (2024). ",
-    "Unraveling the molecular relevance of brain phenotypes: A comparative analysis of null models and test statistics. Neuroimage, 293, 120622.\n",
-    "2. Cao Z (2024). BrainEnrich: Revealing Biological Insights from Imaging-Derived Features through Transcriptomic Enrichment. ",
-    "R package version 1.0, <https://zh1peng.github.io/BrainEnrich/>."
-  )
+  # packageStartupMessage(
+  #   "To cite the package, please use the following reference:\n",
+  #   "1. Cao, Z., Zhan, G., Qin, J., Cupertino, R. B., Ottino-Gonzalez, J., Murphy, A., ... & Garavan, H. (2024). ",
+  #   "Unraveling the molecular relevance of brain phenotypes: A comparative analysis of null models and test statistics. Neuroimage, 293, 120622.\n",
+  #   "2. Cao Z (2024). BrainEnrich: Revealing Biological Insights from Imaging-Derived Features through Transcriptomic Enrichment. ",
+  #   "R package version 1.0, <https://zh1peng.github.io/BrainEnrich/>."
+  # )
   # Uncomment the following lines if you want to dynamically fetch the citation
   # citation_info <- citation(pkgname)
   # packageStartupMessage(capture.output(print(citation_info, style = "text")))
